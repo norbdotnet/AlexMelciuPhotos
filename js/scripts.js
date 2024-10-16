00b67d8da58e93cad0aa236778f5a8ee139e93fd
Norbert Manyi <norbert.manyi@7f.ro>
2024-10-17 01:13:12 +0300

    reorg

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('header.html')
+    fetch('_header.html')
         .then(response => response.text())
         .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data);
+            document.head.innerHTML += data;
+        });
+    fetch('_navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            const navbarContainer  = document.getElementById('amf-navbar-container');
+            if (navbarContainer ) {
+                navbarContainer.innerHTML = data;
+            }
             // Set the active link
             const currentPage = window.location.pathname.split('/').pop();
             const activeLinkId = currentPage === '' ? 'index-link' : `${currentPage.split('.')[0]}-link`;
